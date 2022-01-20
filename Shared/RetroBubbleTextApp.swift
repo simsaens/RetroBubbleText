ce559342d062a758e3a43dbbaf6412d55238db4f
Sim <simeon@twolivesleft.com>
2022-01-20 20:59:48 +1030

    Initial Commit

---
+++ Shared/RetroBubbleTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetroBubbleTextApp.swift
+//  Shared
+//
+//  Created by Sim Saens on 20/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RetroBubbleTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
